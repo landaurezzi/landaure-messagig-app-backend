47beca572ce2f5d3cdc5f5d7c5cf6102883ef53a
landaurezzi <dlandaure@yahoo.com>
2022-04-25 22:05:34 -0600

    another commit

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const app = express()
 const port = process.env.PORT || 9000
 const connection_url = "mongodb+srv://admin:admin@cluster0.aseq5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
+//const Pusher = require("pusher");
+
+const pusher = new Pusher({
+  appId: "1401286",
+  key: "e9eb5617509a018ef074",
+  secret: "a8367666fc9b57e2a192",
+  cluster: "us3",
+  useTLS: true
+});
+
 //Middleware
 app.use(express.json())
 app.use(Cors())
@@ -16,11 +26,32 @@ app.use(Cors())
 //DB config
 mongoose.connect(connection_url, {
     useNewUrlParser: true,
-    useCreateIndex: true,
+    //useCreateIndex: true,
     useUnifiedTopology: true
 })
 
 //API endpoints
+const db = mongoose.connection
+db.once("open", () => {
+    console.log("DB Connected")
+    const msgCollection = db.collection("messagingmessages")
+    const changeStream = msgCollection.watch()
+    changeStream.on('change', change => {
+        console.log(change)
+        if(change.operationType === "insert") {
+            const messageDetails = change.fullDocument
+            pusher.trigger("messages", "inserted", {
+                name: messageDetails.name,
+                message: messageDetails.message,
+                timestamp: messageDetails.timestamp,
+                received: messageDetails.received
+            })
+        }
+        else {
+            console.log('Error triggering Pusher')
+        }
+    })
+})
 app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"))
 
 app.post('/messages/new', (req, res) => {
